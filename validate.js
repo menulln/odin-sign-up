d37d0308bef08f340f565345509618a1aae1c3ea
menulln <39300523+menulln@users.noreply.github.com>
2022-11-30 19:35:41 +0100

    Move code outside if statement to fix scope for other if statements

--- validate.js
+++ validate.js
@@ -3,9 +3,9 @@ const inputElements = document.querySelectorAll('input');
 inputElements.forEach( (input) => {
   input.addEventListener('focusout', (e) => {
     if (!(input.validity.valid) && input.value !== "") {
+      const errorIcon = input.nextElementSibling;
+      const errorMsg = errorIcon.firstChild; 
       if (input.validity.typeMismatch) {
-        const errorIcon = input.nextElementSibling;
-        const errorMsg = errorIcon.firstChild; 
         errorMsg.textContent = 'Wrong format. | name@example.com';
         errorIcon.style.visibility = 'visible';
         errorMsg.style.visibility = 'visible';
