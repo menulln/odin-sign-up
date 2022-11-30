c178de9a89673c9c80db3d0c9043a218dc7257de
menulln <39300523+menulln@users.noreply.github.com>
2022-11-30 19:17:30 +0100

    Add if statements for all possible validation types

--- validate.js
+++ validate.js
@@ -2,6 +2,16 @@ const inputElements = document.querySelectorAll('input');
 
 inputElements.forEach( (input) => {
   input.addEventListener('focusout', (e) => {
+    if (!(input.validity.valid) && input.value !== "") {
+      if (input.validity.typeMismatch) {
 
+      } else if (input.validity.patternMismatch) {
+
+      } else if (input.validity.tooLong) {
+
+      } else if (input.validity.tooShort) {
+        
+      }
+    }
   })
 });
