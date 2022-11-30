0437364d49f5e69954a9c99bde45027f0cbd7cda
menulln <39300523+menulln@users.noreply.github.com>
2022-11-30 19:31:19 +0100

    Set error message textContent and make visible

--- validate.js
+++ validate.js
@@ -4,13 +4,17 @@ inputElements.forEach( (input) => {
   input.addEventListener('focusout', (e) => {
     if (!(input.validity.valid) && input.value !== "") {
       if (input.validity.typeMismatch) {
-
+        const errorIcon = input.nextElementSibling;
+        const errorMsg = errorIcon.firstChild; 
+        errorMsg.textContent = 'Wrong format. | name@example.com';
+        errorIcon.style.visibility = 'visible';
+        errorMsg.style.visibility = 'visible';
       } else if (input.validity.patternMismatch) {
 
       } else if (input.validity.tooLong) {
 
       } else if (input.validity.tooShort) {
-        
+
       }
     }
   })
