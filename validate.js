c9da48cfc11dcc70301041c85566f81defa0b83c
menulln <39300523+menulln@users.noreply.github.com>
2022-11-30 21:04:23 +0100

    Add input event listener to style element if valid

--- validate.js
+++ validate.js
@@ -7,9 +7,20 @@ inputElements.forEach( (input) => {
       const errorMsg = errorIcon.firstChild; 
       if (input.validity.typeMismatch) {
         errorMsg.textContent = 'Wrong format. | name@example.com';
+        errorIcon.classList.remove('valid');
+        errorIcon.classList.add('icon');
         errorIcon.style.visibility = 'visible';
         errorMsg.style.visibility = 'visible';
         input.style.border = '1px solid rgb(199, 70, 70)';
+
+        input.addEventListener('input', (e) => {
+          if (input.validity.valid) {
+            input.style.border = '1px solid rgb(88, 209, 77)';
+            errorIcon.classList.remove('icon');
+            errorIcon.classList.add('valid');
+            errorMsg.textContent = 'Valid email';
+          }
+        });
       } else if (input.validity.patternMismatch) {
 
       } else if (input.validity.tooLong) {
